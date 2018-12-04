53a548875fb297d5c645e6cfab75048c3b88f146
Ioan <ioan.grozea@gmail.com>
2018-12-04 17:40:47 +0200

    asdf

---
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+
+import com.example.demo.entity.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+import java.util.Set;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User>  findUserByEmail(String email);
+    Optional<User>  findUserByUserName(String userName);
+}
