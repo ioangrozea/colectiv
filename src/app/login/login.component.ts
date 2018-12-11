b6afccb05edad2a28d6db58825fd60f7d0096ae0
Cristina <murzacristina@yahoo.ro>
2018-12-11 18:17:07 +0200

    test

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component,NgModule, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,12 @@ import { Component,NgModule, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+
+  btnClickLogin = function() {
+    this.router.navigateByUrl('/profile');
+  }
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
