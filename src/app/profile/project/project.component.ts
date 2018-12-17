04cb89bc24bfe091206f937dd09fdcafa182a271
Maria Cotos <maria.cotos7@yahoo.com>
2018-12-17 18:15:13 +0200

    display skill-profile + change navbar

--- src/app/profile/project/project.component.ts
+++ src/app/profile/project/project.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectComponent implements OnInit {
 
+  projectArray: {title,branche, client, role, period, description: string}[] = [
+    {"title": "Management application","branche": "University", "client":"Audi", "role": "Project owner", "period": "10-10-2010", "description":"Communication with client"},
+    {"title": "2Management application","branche": "2ng University", "client":"Audi", "role": "Project owner", "period": "10-10-2010", "description":"Communication with client"}
+  ];
+
   constructor() { }
 
   ngOnInit() {
--- src/app/profile/skill-profile/skill-profile.component.ts
+++ src/app/profile/skill-profile/skill-profile.component.ts
@@ -7,12 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillProfileComponent implements OnInit {
 
+  categoryArray: {category, listname: string}[] = [
+    {"category": "Programming skills:","listname":"progskillsArray"},
+    {"category": "Soft skills:","listname":"softkillsArray"}
+  ];
 
+  progskillsArray: {skill, level: string}[] = [
+    {"skill": "C++", "level":"beginner"},
+    {"skill": "Java", "level":"beginner"},
+    {"skill": "Python", "level":"expert"},
+    {"skill": "SQL", "level":"beginner"},
+    {"skill": "Java", "level":"beginner"},
+    {"skill": "Python", "level":"expert"},
+    {"skill": "SQL", "level":"beginner"}
+  ];
 
+  softskillsArray: {skill, level: string}[] = [
+    {"skill": "Communication", "level":"intermediate"},
+    {"skill": "Teamwork", "level":"beginner"}
+  ];
   constructor() { }
 
   ngOnInit() {
   }
-  public trainingProgress: 10;
 
+  public trainingProgress: number=10;
 }
+
+
