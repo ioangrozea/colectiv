d263d9a74698dd494c48976372ee0d97bc93a135
teodora <teodora.lupas@gmail.com>
2019-01-15 19:34:59 +0200

    get post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { MitarbeiterProfileComponent } from './mitarbeiter-profile/mitarbeiter-profile.component';
-import { ProfileComponent } from './profile/profile.component';
-import { SkillProfileComponent } from './profile/skill-profile/skill-profile.component';
-import { ReportComponent } from './report/report.component';
-import { ProjectComponent } from './profile/project/project.component';
-import { MainmenuComponent } from './mainmenu/mainmenu.component';
-import { CardComponent } from './profile/card/card.component';
-import { EditMitarbeiterComponent } from './mitarbeiter-profile/edit-mitarbeiter/edit-mitarbeiter.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+import {MitarbeiterProfileComponent} from './mitarbeiter-profile/mitarbeiter-profile.component';
+import {ProfileComponent} from './profile/profile.component';
+import {SkillProfileComponent} from './profile/skill-profile/skill-profile.component';
+import {ReportComponent} from './report/report.component';
+import {ProjectComponent} from './profile/project/project.component';
+import {MainmenuComponent} from './mainmenu/mainmenu.component';
+import {CardComponent} from './profile/card/card.component';
+import {EditMitarbeiterComponent} from './mitarbeiter-profile/edit-mitarbeiter/edit-mitarbeiter.component';
+import {ContentService} from './shared/ContentService';
+import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
@@ -29,9 +31,13 @@ import { EditMitarbeiterComponent } from './mitarbeiter-profile/edit-mitarbeiter
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    ContentService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/mitarbeiter-profile/mitarbeiter-profile.component.ts
+++ src/app/mitarbeiter-profile/mitarbeiter-profile.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Data, Router} from '@angular/router';
+import {User} from '../shared/entity/User';
+import {ContentService} from '../shared/ContentService';
 
 @Component({
   selector: 'app-mitarbeiter-profile',
@@ -8,7 +10,9 @@ import {Data, Router} from '@angular/router';
 })
 export class MitarbeiterProfileComponent implements OnInit {
 
-  mitarbeiterArray: {id, name: string, photo: string, consulting_level: string, region: string}[] = [
+  user: User;
+  users: Array<User>;
+  mitarbeiterArray: { id, name: string, photo: string, consulting_level: string, region: string }[] = [
     {'id': 0, 'name': 'Cotos Maria', 'photo': 'emp1.jpg', 'consulting_level': 'Associate IT Consultant', 'region': 'Suceava'},
     {'id': 1, 'name': 'Szappanyos Szdionia', 'photo': 'image.jpg', 'consulting_level': 'IT Consultant', 'region': 'Medias'},
     {'id': 2, 'name': 'Lupas Teodora', 'photo': 'emp1.jpg', 'consulting_level': 'Scrum master', 'region': 'Bucuresti'},
@@ -16,18 +20,23 @@ export class MitarbeiterProfileComponent implements OnInit {
     {'id': 4, 'name': 'Lung Oana', 'photo': 'emp1.jpg', 'consulting_level': 'Associate IT Consultant', 'region': 'Tirgu-Mures'}
   ];
 
-  constructor(private router: Router) { }
-
-  ngOnInit() {
+  constructor(private router: Router, private contentService: ContentService) {
   }
 
- editMitarbeiter(id, name, consulting_level, region, photo) {
-   localStorage.setItem('id', id);
-   localStorage.setItem('name', name);
-   localStorage.setItem('consulting_level', consulting_level);
-   localStorage.setItem('region', region);
-   localStorage.setItem('photo', photo);
-   this.router.navigate(['/editSubordinate']);
- }
+  ngOnInit() {
+    this.contentService.addUser();
+    this.contentService.getAllUsers().subscribe(res => {
+      console.log(res);
+    });
+  }
+
+  editMitarbeiter(id, name, consulting_level, region, photo) {
+    localStorage.setItem('id', id);
+    localStorage.setItem('name', name);
+    localStorage.setItem('consulting_level', consulting_level);
+    localStorage.setItem('region', region);
+    localStorage.setItem('photo', photo);
+    this.router.navigate(['/editSubordinate']);
+  }
 
 }
